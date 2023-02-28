763246d6bcadc7d221a98e4439a14ff99bef0e50
albertoBarl <alberto.barletta00@gmail.com>
2023-02-28 21:33:46 +0100

    changed extends from layouts.app to .admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div class="d-flex justify-content-center aling-items-center p-5">
-        <h1>WELCOME TO <span class="text-danger">LARAVEL_AUTH</span> PROJECT</h1>
+        <h1>WELCOME TO <span class="text-danger">BOOLPRESS</span> PROJECT</h1>
     </div>
 @endsection
