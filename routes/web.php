03d37869a5c7a4bcf72740fcf8f85349e1d99338
albertoBarl <alberto.barletta00@gmail.com>
2023-02-28 22:16:55 +0100

    added route::resource projects

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,6 +23,7 @@ Route::get('/', function () {
 // route for dashboard
 Route::middleware(["auth", "verified"])->name("admin.")->prefix("admin")->group(function () {
     Route::get("/", [DashboardController::class, "index"])->name("dashboard");
+    Route::resource("projects", ProjectController::class)->parameters(["projects" => "project:slug"]);
 });
 
 Route::middleware('auth')->group(function () {
