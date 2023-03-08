67f0894adc7c3ef024548f7f40d4909cb4fcbb00
albertoBarl <alberto.barletta00@gmail.com>
2023-03-08 20:25:48 +0100

    fixed edit function

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
     <div id="edit-project">
-        <form action="{{ route('admin.projects.update', ['project' => $project->id]) }}" method="POST"
+        <form action="{{ route('admin.projects.update', ['project' => $project->slug]) }}" method="POST"
             class="d-flex flex-column justify-content-center p-5">
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title">
+                <input type="text" class="form-control" name="title" value="{{ old('title', $project) }}">
             </div>
             <div class="mb-3">
                 <label for="content" class="form-label">Content</label>
-                <textarea rows="5" class="form-control" name="content"></textarea>
+                <textarea rows="5" class="form-control" name="content">{{ old('content', $project) }}</textarea>
             </div>
             <div class="form-group">
-                <button type="submit" class="btn btn-primary">Add project</button>
+                <button type="submit" class="btn btn-primary">Save project</button>
             </div>
         </form>
 
